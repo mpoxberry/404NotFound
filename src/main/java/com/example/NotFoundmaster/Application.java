d209b198a3824965378631a187d253363e8be26d
zbuz <bizuayehu.debele@qvc.com>
2018-10-29 13:23:52 -0400

    add template model

--- src/main/java/com/example/NotFoundmaster/Application.java
+++ src/main/java/com/example/NotFoundmaster/Application.java
@@ -1,12 +1,26 @@
 package com.example.NotFoundmaster;
 
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class Application {
+
+	@Autowired
+	private RuntimeService runtimeService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@EventListener
+	private void processPostDeploy(PostDeployEvent event) {
+		runtimeService.startProcessInstanceByKey("loanApproval");
 	}
 }
