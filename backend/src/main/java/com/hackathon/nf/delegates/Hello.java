635065bc72068f867824fa8553271bc30cd870f9
ZachHubbs <Zachary.T.Hubbs@qvc.com>
2018-10-29 18:34:10 -0400

    Updated Simple Camunda Model

--- backend/src/main/java/com/hackathon/nf/delegates/Hello.java
+++ backend/src/main/java/com/hackathon/nf/delegates/Hello.java
@@ -1,13 +1,19 @@
 package com.hackathon.nf.delegates;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.Expression;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 public class Hello implements JavaDelegate{
+    
+    private Expression text1;
 
+    
     @Override
     public void execute(DelegateExecution execution) throws Exception {
-        System.out.println("Hello");
+        
+        String value1 = (String) text1.getValue(execution);
+        System.out.println("Hello "+ value1);
         
     }
 
