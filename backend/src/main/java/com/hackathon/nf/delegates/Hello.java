1615236e9ec0cfc5a4505e8477a3516b819c19f9
Nate Hochstetler <Nathaniel.Hochstetler@qvc.com>
2018-10-29 19:49:28 -0400

    one teeny thing works

--- backend/src/main/java/com/hackathon/nf/delegates/Hello.java
+++ backend/src/main/java/com/hackathon/nf/delegates/Hello.java
@@ -6,15 +6,9 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 public class Hello implements JavaDelegate{
     
-    private Expression text1;
-
-    
     @Override
     public void execute(DelegateExecution execution) throws Exception {
-        
-        String value1 = (String) text1.getValue(execution);
-        System.out.println("Hello "+ value1);
-        
-    }
+        System.out.println("Processing request by '" + execution.getVariable("Input_00qmfa7") + "'...");
+      }
 
 }
