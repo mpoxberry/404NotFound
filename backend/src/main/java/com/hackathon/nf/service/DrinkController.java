7f322e135fc10dd9896eb9a5d97665e77cf6830c
mpoxberry <matthew.oxberry@qvc.com>
2018-10-29 19:30:05 -0400

    hard coded list

--- backend/src/main/java/com/hackathon/nf/service/DrinkController.java
+++ backend/src/main/java/com/hackathon/nf/service/DrinkController.java
@@ -1,6 +1,7 @@
 package com.hackathon.nf.service;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -65,6 +68,20 @@ public class DrinkController {
 		
 		HttpEntity<String> response = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, entity, String.class);
 		return response.getBody();
+	}
+	
+	@RequestMapping(path = "/pairings")
+	@ResponseStatus(HttpStatus.OK)
+	public Collection<FoodAlcoholPair> getPairings() {
+		List<FoodAlcoholPair> fap = new ArrayList<>();
+		
+		fap.add(new FoodAlcoholPair("Italian", "Amaretto"));
+		fap.add(new FoodAlcoholPair("Mexican", "Tequila"));
+		fap.add(new FoodAlcoholPair("Mexican", "Brandy"));
+		fap.add(new FoodAlcoholPair("American", "Whiskey"));
+		fap.add(new FoodAlcoholPair("American", "Bourbon"));
+		
+		return fap; 
 	}
 
 }
