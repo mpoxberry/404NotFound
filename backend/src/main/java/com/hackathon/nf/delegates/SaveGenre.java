2edf6a8e0ed89068b136a5169470b991fcde719c
ZachHubbs <Zachary.T.Hubbs@qvc.com>
2018-10-29 23:40:39 -0400

    Added additional Delegates and added locking steps

--- backend/src/main/java/com/hackathon/nf/delegates/SaveGenre.java
+++ backend/src/main/java/com/hackathon/nf/delegates/SaveGenre.java
@@ -4,11 +4,11 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.Expression;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
-public class Hello implements JavaDelegate{
+public class SaveGenre implements JavaDelegate{
     
     @Override
     public void execute(DelegateExecution execution) throws Exception {
-        System.out.println("Processing request by '" + execution.getVariable("food") + "'...");
+        System.out.println("Processing request by '" + execution.getVariable("genre") + "'...");
       }
 
 }
