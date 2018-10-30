403bc9fef69b86ac43950c9aa6232e9ed7e35291
Nate Hochstetler <Nathaniel.Hochstetler@qvc.com>
2018-10-30 08:36:32 -0400

    user controller

--- backend/src/main/java/com/hackathon/nf/service/WorkflowController.java
+++ backend/src/main/java/com/hackathon/nf/service/WorkflowController.java
@@ -2,9 +2,13 @@ package com.hackathon.nf.service;
 
 import org.camunda.bpm.engine.RuntimeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import com.hackathon.nf.model.User;
 
 @RestController
 @RequestMapping("/api/workflow")
@@ -15,9 +19,21 @@ public class WorkflowController {
 
     @Autowired
     RestTemplate restTemplate;
+    
+    CategorySelectionService service = new CategorySelectionService();
 
     @RequestMapping("/start")
     public void start() {
         runtimeService.startProcessInstanceByKey("Process_2");
+    }
+    
+    @RequestMapping("/movies")
+    public void getMovies() {
+        service.findMaxSelectedRestaurant();
+    }
+    
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public void saveUser(@RequestBody User user) {
+        service.saveUser(user);
     }
 }
